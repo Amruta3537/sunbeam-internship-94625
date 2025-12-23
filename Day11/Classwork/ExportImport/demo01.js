4ffb49313badda601918d226e5ea263b85082688
Amruta Dhumal <adhumal3537@gmail.com>
2025-12-23 19:53:31 +0530

    Day11- Assignment + Classwork done!

---
+++ Day11/Classwork/ExportImport/demo01.js
@@ -0,0 +1,12 @@
+import add from './math.mjs'
+import { sub, mul } from './math.mjs'
+
+
+// console.log(math)
+// math.add(10, 20)
+// math.mul(11, 22)
+
+
+console.log(add)
+console.log(sub)
+console.log(mul)
